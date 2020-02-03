d3f5c7ffe0d42a3a4b092cfc4b4f976c43a462eb
Fustile <fustile99@gmail.com>
2020-02-03 18:13:49 +0300

    code style fixes

--- js/app.js
+++ js/app.js
@@ -1,64 +1,63 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     var width = $(document).width()
 
     ShrinkH2()
 
-    $( window ).resize(function(){
+    $( window ).resize(function() {
         width = $(document).width()
-        if (width <= 1155 && width >= 990){
+        if (width <= 1155 && width >= 990) {
             ShrinkH2()
         }
-        else{
-            $(".cardTitle").css('font-size', '30px')
+        else {
+            $(".card__title").css('font-size', '30px')
         }
 
     })
 
-    function ShrinkH2(){
-        if ( $(".card1").width() == $(".card2").width() && $(".card2").width() == $(".card3").width())
+    function ShrinkH2() {
+        if ( $(".card_1").width() == $(".card_2").width() && $(".card_2").width() == $(".card_3").width())
             return
-        else{
-            $(".cardTitle").css('font-size', '25px')
+        else {
+            $(".card__title").css('font-size', '25px')
         }
     }
 
     var bookedRoom
-    $(document).on("click", ".buttonActive", function(){
+    $(document).on("click", ".button_active", function() {
         bookedRoom = $(this).parent().parent(".card")
     })
-    $(".card").on({
-        mouseleave: function (){
-            $(bookedRoom).addClass("cardDisabled")
-            $(bookedRoom).find(".cardImage").addClass("imageDisabled")
-            $(bookedRoom).find(".cardInfo").addClass("cardInfoDisabled")
-            $(bookedRoom).find(".cardTitle").addClass("cardTitleDisabled")
-            $(bookedRoom).find(".cardInfoText").addClass("cardInfoTextDisabled")
-            $(bookedRoom).find(".cardPrice").addClass("cardPriceDisabled")
-            $(bookedRoom).find(".cardPriceText").addClass("cardPriceTextDisabled")
-            $(bookedRoom).find(".price").addClass("priceDisabled")
-            $(bookedRoom).find(".cardButton").removeClass("buttonActive")
-            $(bookedRoom).find(".cardButton").addClass("cardButtonDisabled")
-            // $(bookedRoom).find(".buttonText").addClass("buttonTextDisabled")
-            $(bookedRoom).find(".buttonText").hide()
-            $(bookedRoom).find(".buttonTextDisabled").show()
+    $(".card").on( {
+        mouseleave: function () {
+            $(bookedRoom).addClass("card_disabled")
+            $(bookedRoom).find(".card__image").addClass("image_disabled")
+            $(bookedRoom).find(".card__info").addClass("card__info_disabled")
+            $(bookedRoom).find(".card__title").addClass("card__title_disabled")
+            $(bookedRoom).find(".info__text").addClass("info__text_disabled")
+            $(bookedRoom).find(".card__price").addClass("card__price_disabled")
+            $(bookedRoom).find(".price__text").addClass("price__text_disabled")
+            $(bookedRoom).find(".price").addClass("price_disabled")
+            $(bookedRoom).find(".price__button").removeClass("button_active")
+            $(bookedRoom).find(".price__button").addClass("price__button_disabled")
+            $(bookedRoom).find(".button__text").hide()
+            $(bookedRoom).find(".button__text_disabled").show()
 
             bookedRoom = null
         }
     })
-    $(document).on("click", ".cardDisabled", function(){
-        $(this).removeClass("cardDisabled")
-        $(this).find(".cardImage").removeClass("imageDisabled")
-        $(this).find(".cardInfo").removeClass("cardInfoDisabled")
-        $(this).find(".cardTitle").removeClass("cardTitleDisabled")
-        $(this).find(".cardInfoText").removeClass("cardInfoTextDisabled")
-        $(this).find(".cardPrice").removeClass("cardPriceDisabled")
-        $(this).find(".cardPriceText").removeClass("cardPriceTextDisabled")
-        $(this).find(".price").removeClass("priceDisabled")
-        $(this).find(".cardButton").removeClass("cardButtonDisabled")
-        $(this).find(".cardButton").addClass("buttonActive")
-        $(this).find(".buttonTextDisabled").hide()
-        $(this).find(".buttonText").show()
+    $(document).on("click", ".card_disabled", function() {
+        $(this).removeClass("card_disabled")
+        $(this).find(".card__image").removeClass("image_disabled")
+        $(this).find(".card__info").removeClass("card__info_disabled")
+        $(this).find(".card__title").removeClass("card__title_disabled")
+        $(this).find(".info__text").removeClass("info__text_disabled")
+        $(this).find(".card__price").removeClass("card__price_disabled")
+        $(this).find(".price__text").removeClass("price__text_disabled")
+        $(this).find(".price").removeClass("price_disabled")
+        $(this).find(".price__button").removeClass("price__button_disabled")
+        $(this).find(".price__button").addClass("button_active")
+        $(this).find(".button__text_disabled").hide()
+        $(this).find(".button__text").show()
     })
 
 
