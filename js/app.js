40af744d401086482feb0a07658203b1c2f609f8
Fustile <fustile99@gmail.com>
2020-02-08 18:38:53 +0300

    added html tags, fixed fonts,classes

--- js/app.js
+++ js/app.js
@@ -26,34 +26,34 @@ $(document).ready(function() {
     });
     $(".card").on( {
         mouseleave: function () {
-            $(bookedRoom).addClass("card_disabled");
-            $(bookedRoom).find(".card__image").addClass("image_disabled");
-            $(bookedRoom).find(".card__info").addClass("card__info_disabled");
-            $(bookedRoom).find(".card__title").addClass("card__title_disabled");
-            $(bookedRoom).find(".info__text").addClass("info__text_disabled");
-            $(bookedRoom).find(".card__price").addClass("card__price_disabled");
-            $(bookedRoom).find(".price__text").addClass("price__text_disabled");
-            $(bookedRoom).find(".price").addClass("price_disabled");
+            $(bookedRoom).addClass("card--disabled");
+            $(bookedRoom).find(".card__image").addClass("image--disabled");
+            $(bookedRoom).find(".card__info").addClass("card__info--disabled");
+            $(bookedRoom).find(".card__title").addClass("card__title--disabled");
+            $(bookedRoom).find(".info__text").addClass("info__text--disabled");
+            $(bookedRoom).find(".card__price").addClass("card__price--disabled");
+            $(bookedRoom).find(".price__text").addClass("price__text--disabled");
+            $(bookedRoom).find(".price").addClass("price--disabled");
             $(bookedRoom).find(".price__button").removeClass("button_active");
-            $(bookedRoom).find(".price__button").addClass("price__button_disabled");
+            $(bookedRoom).find(".price__button").addClass("button--disabled");
             $(bookedRoom).find(".button__text").hide();
-            $(bookedRoom).find(".button__text_disabled").show();
+            $(bookedRoom).find(".button__text--disabled").show();
 
             bookedRoom = null;
         }
     });
-    $(document).on("click", ".card_disabled", function() {
-        $(this).removeClass("card_disabled");
-        $(this).find(".card__image").removeClass("image_disabled");
-        $(this).find(".card__info").removeClass("card__info_disabled");
-        $(this).find(".card__title").removeClass("card__title_disabled");
-        $(this).find(".info__text").removeClass("info__text_disabled");
-        $(this).find(".card__price").removeClass("card__price_disabled");
-        $(this).find(".price__text").removeClass("price__text_disabled");
-        $(this).find(".price").removeClass("price_disabled");
-        $(this).find(".price__button").removeClass("price__button_disabled");
+    $(document).on("click", ".card--disabled", function() {
+        $(this).removeClass("card--disabled");
+        $(this).find(".card__image").removeClass("image--disabled");
+        $(this).find(".card__info").removeClass("card__info--disabled");
+        $(this).find(".card__title").removeClass("card__title--disabled");
+        $(this).find(".info__text").removeClass("info__text--disabled");
+        $(this).find(".card__price").removeClass("card__price--disabled");
+        $(this).find(".price__text").removeClass("price__text--disabled");
+        $(this).find(".price").removeClass("price--disabled");
+        $(this).find(".price__button").removeClass("button--disabled");
         $(this).find(".price__button").addClass("button_active");
-        $(this).find(".button__text_disabled").hide();
+        $(this).find(".button__text--disabled").hide();
         $(this).find(".button__text").show();
     });
 });
