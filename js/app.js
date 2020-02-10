af48bbc2920fc8db0e03d81b32e185574641af30
Fustile <fustile99@gmail.com>
2020-02-10 20:48:57 +0300

    fixed js

--- js/app.js
+++ js/app.js
@@ -1,61 +1,57 @@
 $(document).ready(function() {
 
-    // let width = $(document).width();
-    // let widthHigh = 1155;
-    // let widthLow = 990;
+    let bookedRoom = null;
+    let cardNum;
 
-    // ShrinkH2();
-
-    // $( window ).resize(function() {
-    //     width = $(document).width();
-    //     ShrinkH2();
-    // });
-
-    // function ShrinkH2() {
-    //     if (widthHigh >= width && width >= widthLow) {
-    //         $(".card__title").css("font-size", "25px");
-    //     }
-    //     else {
-    //         $(".card__title").css("font-size", "30px");
-    //     }
-    // }
-
-    let bookedRoom;
-    let cardNum
     $(document).on("click", ".button--active", function() {
         bookedRoom = $(this).parent().parent(".card");
     });
-    $(".card").on( {
-        mouseleave: function () {
-            $(bookedRoom).removeClass("card--active");
-            $(bookedRoom).addClass("card--disabled");
-            $(bookedRoom).find(".price__button").removeClass("button--active")
-            $(bookedRoom).find('*').addClass("disabled");
-            $(bookedRoom).find(".button__text").hide();
-            $(bookedRoom).find(".button__text--disabled").show();
 
-            cardNum = $(bookedRoom).parent().index();
-            cardNum++
-            if (cardNum > 0) {
-                $(bookedRoom).find(".card__image").attr("src","img/disabled/card" + cardNum +".png")
-            };
-            bookedRoom = null;
-        }
+    $(".card").mouseleave(function () {
+        if (bookedRoom === null)
+        return;
+        CardDisable(bookedRoom);
+        ImgDisable(bookedRoom);
+        bookedRoom = null;
     });
-
 
     $(document).on("click", ".card--disabled", function() {
-        $(this).removeClass("card--disabled");
-        $(this).addClass("card--active");
-        $(this).find(".price__button").addClass("button--active")
-        $(this).find('*').removeClass("disabled");
-        $(this).find(".button__text--disabled").hide();
-        $(this).find(".button__text").show();
-
-        cardNum = $(this).parent().index();
-        cardNum++
-        if (cardNum > 0) {
-            $(this).find(".card__image").attr("src","img/card" + cardNum +".png")
-        };
+        let card = $(this);
+        CardEnable(card);
+        ImgEnable(card);
     });
+
+    function CardDisable (bookedRoom) {
+        $(bookedRoom).removeClass("card--active");
+        $(bookedRoom).addClass("card--disabled");
+        $(bookedRoom).find('*').addClass("disabled");
+        $(bookedRoom).find(".price__button").removeClass("button--active");
+        $(bookedRoom).find(".button__text").hide();
+        $(bookedRoom).find(".button__text--disabled").show();
+    }
+    
+    function CardEnable (card) {
+        $(card).removeClass("card--disabled");
+        $(card).addClass("card--active");
+        $(card).find('*').removeClass("disabled");
+        $(card).find(".price__button").addClass("button--active");
+        $(card).find(".button__text--disabled").hide();
+        $(card).find(".button__text").show();
+    }
+    
+    function ImgDisable (bookedRoom) {
+        cardNum = $(bookedRoom).parent().index();
+        cardNum++;
+        if (cardNum > 0) {
+            $(bookedRoom).find(".card__image").attr("src","img/disabled/card" + cardNum +".png");
+        }
+    }
+    
+    function ImgEnable (card) {
+        cardNum = $(card).parent().index();
+        cardNum++;
+        if (cardNum > 0) {
+            $(card).find(".card__image").attr("src","img/card" + cardNum +".png");
+        }
+    }
 });
